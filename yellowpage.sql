637f1e81d8f0c67ee48f576a96c9382ccda1bf9a
anytizer <anytizer@gmail.com>
2020-04-19 17:32:37 -0600

    Double Quote fields and tables

--- yellowpage.sql
+++ yellowpage.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS "persons";
 CREATE TABLE "persons" (
 	"person_id"	TEXT NOT NULL,
 	"person_title"	TEXT NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE "persons" (
 	PRIMARY KEY("person_id")
 );
 
-DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS "addresses";
 CREATE TABLE "addresses" (
 	"address_id"	TEXT NOT NULL,
 	"person_id" 	TEXT NOT NULL,
@@ -19,11 +19,11 @@ CREATE TABLE "addresses" (
 	"address_postalcode"	TEXT NOT NULL,
 	"address_country"	TEXT NOT NULL,
 	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY (person_id) REFERENCES persons(person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY("address_id")
 );
 
-DROP TABLE IF EXISTS credentials;
+DROP TABLE IF EXISTS "credentials";
 CREATE TABLE "credentials" (
 	"credential_id"	TEXT NOT NULL,
 	"person_id" 	TEXT NOT NULL,
@@ -32,11 +32,11 @@ CREATE TABLE "credentials" (
 	"credential_question"	TEXT NOT NULL,
 	"credential_answer"	TEXT NOT NULL,
 	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY (person_id) REFERENCES persons(person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY("credential_id")
 );
 
-DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS "contacts";
 CREATE TABLE "contacts" (
 	"contact_id"	TEXT NOT NULL,
 	"person_id" 	TEXT NOT NULL,
@@ -45,11 +45,11 @@ CREATE TABLE "contacts" (
 	"contact_phone"	TEXT NOT NULL,
 	"contact_fax"	TEXT NOT NULL,
 	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY (person_id) REFERENCES persons(person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY("contact_id")
 );
 
-DROP TABLE IF EXISTS businesses;
+DROP TABLE IF EXISTS "businesses";
 CREATE TABLE "businesses" (
 	"business_id"	TEXT NOT NULL,
 	"person_id" 	TEXT NOT NULL,
@@ -58,11 +58,11 @@ CREATE TABLE "businesses" (
 	"business_registration"	TEXT NOT NULL,
 	"established_on" TEXT NOT NULL,
 	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY (person_id) REFERENCES persons(person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY("business_id")
 );
 
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS "categories";
 CREATE TABLE "categories" (
 	"category_id"	TEXT NOT NULL,
 	"person_id" 	TEXT NOT NULL,
@@ -72,6 +72,6 @@ CREATE TABLE "categories" (
 	"category_lastname"	TEXT NOT NULL,
 	"category_nickname"	TEXT NOT NULL,
 	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY (person_id) REFERENCES persons(person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY("category_id")
 );
