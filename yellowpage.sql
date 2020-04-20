3c58270103e9bf3532e36e041511be2cf31fdb02
anytizer <anytizer@gmail.com>
2020-04-19 18:00:19 -0600

    Fixes

--- yellowpage.sql
+++ yellowpage.sql
@@ -1,6 +1,16 @@
+DROP TABLE IF EXISTS "categories";
+CREATE TABLE "categories" (
+	"category_id"	TEXT NOT NULL,
+	"category_name"	TEXT NOT NULL,
+	"category_description"	TEXT NOT NULL,
+	"is_visible"	TEXT NOT NULL,
+	PRIMARY KEY("category_id")
+);
+
 DROP TABLE IF EXISTS "persons";
 CREATE TABLE "persons" (
 	"person_id"	TEXT NOT NULL,
+	"person_code"	TEXT NOT NULL,
 	"person_title"	TEXT NOT NULL,
 	"person_firstname"	TEXT NOT NULL,
 	"person_middlename"	TEXT NOT NULL,
@@ -9,11 +19,33 @@ CREATE TABLE "persons" (
 	PRIMARY KEY("person_id")
 );
 
+DROP TABLE IF EXISTS "businesses";
+CREATE TABLE "businesses" (
+	"business_id"	TEXT NOT NULL,
+	"person_id" 	TEXT NOT NULL,
+	"business_name"	TEXT NOT NULL,
+	"business_license"	TEXT NOT NULL,
+	"business_registration"	TEXT NOT NULL,
+	"established_on" TEXT NOT NULL,
+	"is_visible"	TEXT NOT NULL,
+	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY("business_id")
+);
+
+DROP TABLE IF EXISTS "business_categories";
+CREATE TABLE "business_categories" (
+	"business_category_id"	TEXT NOT NULL,
+	"business_id"	TEXT NOT NULL,
+    "category_id"	TEXT NOT NULL,
+	PRIMARY KEY("business_category_id")
+);
+
 DROP TABLE IF EXISTS "addresses";
 CREATE TABLE "addresses" (
 	"address_id"	TEXT NOT NULL,
 	"person_id" 	TEXT NOT NULL,
-	"address_address"	TEXT NOT NULL,
+	"address_name"	TEXT NOT NULL,
+    "address_address"	TEXT NOT NULL,
 	"address_city"	TEXT NOT NULL,
 	"address_state"	TEXT NOT NULL,
 	"address_postalcode"	TEXT NOT NULL,
@@ -49,29 +81,4 @@ CREATE TABLE "contacts" (
 	PRIMARY KEY("contact_id")
 );
 
-DROP TABLE IF EXISTS "businesses";
-CREATE TABLE "businesses" (
-	"business_id"	TEXT NOT NULL,
-	"person_id" 	TEXT NOT NULL,
-	"business_name"	TEXT NOT NULL,
-	"business_license"	TEXT NOT NULL,
-	"business_registration"	TEXT NOT NULL,
-	"established_on" TEXT NOT NULL,
-	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("business_id")
-);
 
-DROP TABLE IF EXISTS "categories";
-CREATE TABLE "categories" (
-	"category_id"	TEXT NOT NULL,
-	"person_id" 	TEXT NOT NULL,
-	"category_title"	TEXT NOT NULL,
-	"category_firstname"	TEXT NOT NULL,
-	"category_middlename"	TEXT NOT NULL,
-	"category_lastname"	TEXT NOT NULL,
-	"category_nickname"	TEXT NOT NULL,
-	"is_visible"	TEXT NOT NULL,
-	FOREIGN KEY ("person_id") REFERENCES "persons"("person_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("category_id")
-);
